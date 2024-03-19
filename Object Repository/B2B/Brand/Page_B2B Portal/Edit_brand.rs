fb2adf892074572222750a0f6d2d4b54a4392f69
ryan.ananda <ryan.ananda@503-00047.modena.com>
2024-03-19 14:05:44 +0700

    test21

--- Object Repository/B2B/Brand/Page_B2B Portal/Edit_brand.rs
+++ Object Repository/B2B/Brand/Page_B2B Portal/Edit_brand.rs
@@ -6,12 +6,16 @@
    <elementGuidId>f5c7292e-7ee9-4072-91e4-055f9921bc8a</elementGuidId>
    <selectorCollection>
       <entry>
+         <key>CSS</key>
+         <value>a.rounded-circle > div</value>
+      </entry>
+      <entry>
          <key>XPATH</key>
          <value>//td[4]/div/a/div</value>
       </entry>
       <entry>
-         <key>CSS</key>
-         <value>a.rounded-circle > div</value>
+         <key>BASIC</key>
+         <value>id(&quot;mTableId_1422381&quot;)/div[@class=&quot;row&quot;]/div[@class=&quot;col-lg-12 col-md-12 col-12 mtable-container&quot;]/div[1]/table[@class=&quot;table table-condensed&quot;]/tbody[1]/tr[1]/td[@class=&quot;b2b-td&quot;]/div[1]/a[@class=&quot;rounded-circle&quot;]/div[1]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
@@ -22,7 +26,7 @@
       <name>tag</name>
       <type>Main</type>
       <value>div</value>
-      <webElementGuid>3ba53986-a38e-445f-8e37-e85e4ce4fc55</webElementGuid>
+      <webElementGuid>2dedde66-22af-4fb1-bc9b-aac1ed5be9f5</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -30,7 +34,7 @@
       <name>xpath</name>
       <type>Main</type>
       <value>id(&quot;mTableId_1422381&quot;)/div[@class=&quot;row&quot;]/div[@class=&quot;col-lg-12 col-md-12 col-12 mtable-container&quot;]/div[1]/table[@class=&quot;table table-condensed&quot;]/tbody[1]/tr[1]/td[@class=&quot;b2b-td&quot;]/div[1]/a[@class=&quot;rounded-circle&quot;]/div[1]</value>
-      <webElementGuid>0b139f0d-beb4-4a4c-858f-0b204166b4a8</webElementGuid>
+      <webElementGuid>4eec14cb-596d-4199-8143-b7a7aa962ab3</webElementGuid>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
@@ -38,7 +42,7 @@
       <name>xpath:idRelative</name>
       <type>Main</type>
       <value>//div[@id='mTableId_1422381']/div[3]/div/div/table/tbody/tr/td[4]/div/a/div</value>
-      <webElementGuid>1246673f-3724-4548-969a-b814d9dd0b6b</webElementGuid>
+      <webElementGuid>5c1ec6d9-bd03-4c08-b5fe-762b1348243b</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -46,7 +50,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Active'])[1]/following::div[2]</value>
-      <webElementGuid>7af25038-452d-4f26-be30-baf719cac97c</webElementGuid>
+      <webElementGuid>8ff68ae1-c91e-4569-9bad-a44a3c09e0a5</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -54,7 +58,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='LOGITECH'])[1]/following::div[3]</value>
-      <webElementGuid>6d3007ee-7027-4351-bdcd-a9dfecc426ad</webElementGuid>
+      <webElementGuid>1fe5fb02-9f09-49b4-b104-8243253c35c6</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -62,7 +66,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='LENOVO'])[1]/preceding::div[2]</value>
-      <webElementGuid>2a62072a-57f0-435f-acae-f1d96488e056</webElementGuid>
+      <webElementGuid>1e35f7ed-45dd-4005-bdc4-49358e3dbaeb</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -70,7 +74,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Active'])[2]/preceding::div[2]</value>
-      <webElementGuid>ad521d0b-1d8a-4168-a0dc-17601bf167a4</webElementGuid>
+      <webElementGuid>dceed31d-e3c4-4906-ab68-543c105bc37a</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -78,6 +82,6 @@
       <name>xpath:position</name>
       <type>Main</type>
       <value>//td[4]/div/a/div</value>
-      <webElementGuid>a746c869-6f4c-4c6a-8560-4f1cc68f517b</webElementGuid>
+      <webElementGuid>f4234c44-84a1-4467-9f9f-b3da98ba3214</webElementGuid>
    </webElementXpaths>
 </WebElementEntity>
