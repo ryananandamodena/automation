aa0aa71e6f4491704504d1151892a21b2e147af3
ErzaModena <erza.akbar@modena.com>
2024-07-22 10:32:58 +0700

    Add files via upload
    Add BRAVA Erza

---
+++ Object Repository/Object Repository Erza/BRAVA - Erza/Generate QR Code/Page_MODENA Energy/i_Sign In_fa fa-lock.rs
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>i_Sign In_fa fa-lock</name>
+   <tag></tag>
+   <elementGuidId>6787ab4b-4f5c-47b7-93ab-191f22458b8d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='root']/div/div[2]/div/form/div/div[2]/div[5]/div/button/i</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>i.fa.fa-lock</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>id(&quot;app&quot;)/div[@class=&quot;section&quot;]/div[2]/ul[@class=&quot;sortable parent ui-sortable&quot;]/li[@class=&quot;ui-sortable-handle&quot;]/span[@class=&quot;wrapper&quot;]/div[@class=&quot;toggleInteraction&quot;]/div[@class=&quot;dropdown&quot;]/a[@class=&quot;list-icons-item&quot;]/i[@class=&quot;fi fi-rr-menu-dots-vertical&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=button[name=&quot; Sign In&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>i</value>
+      <webElementGuid>bc8642fc-7d1d-454f-b575-9e51aa246bd0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>fa fa-lock</value>
+      <webElementGuid>52e3e1a8-6f16-40d0-9b5b-cb4b5e346c59</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;root&quot;)/div[@class=&quot;login-container&quot;]/div[@class=&quot;login-form&quot;]/div[@class=&quot;d-flex&quot;]/form[1]/div[@class=&quot;row&quot;]/div[@class=&quot;col-10&quot;]/div[@class=&quot;form-group fade-in&quot;]/div[1]/button[@class=&quot;btn btn-block btn-dark btn-lg&quot;]/i[@class=&quot;fa fa-lock&quot;]</value>
+      <webElementGuid>0f180493-34fd-4ef4-97c0-06fffde4e3d1</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='root']/div/div[2]/div/form/div/div[2]/div[5]/div/button/i</value>
+      <webElementGuid>924c2741-db32-4028-96aa-f25247d9ed3b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//button/i</value>
+      <webElementGuid>9ad1b6c3-f0c1-439b-acc6-16e5fb38a9c2</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Object Repository Erza/BRAVA - Erza/Insert Product/Page_Home - CSMS/a_Create Part Number.rs
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Create Part Number</name>
+   <tag></tag>
+   <elementGuidId>752f0109-518e-4451-b370-2ecf115e8f8f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>li.dropdown.open > ul.dropdown-menu > li.dropdown-submenu > ul.dropdown-menu > li > a</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(text(),'Create Part Number')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;Create Part Number&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>2fc0ea2f-7c32-438a-8995-238f3251a871</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>http://192.168.3.87:5057/part/create_part_number</value>
+      <webElementGuid>badb463c-4936-437a-92bd-328021b6dd6d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Create Part Number</value>
+      <webElementGuid>3319d684-831e-4f00-a281-0328b7f2cf01</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;navbar navbar-inverse navbar-fixed-top&quot;]/div[@class=&quot;container-fluid&quot;]/div[@class=&quot;nav-collapse navbar-collapse&quot;]/ul[@class=&quot;nav navbar-nav&quot;]/li[@class=&quot;dropdown open&quot;]/ul[@class=&quot;dropdown-menu&quot;]/li[@class=&quot;dropdown-submenu&quot;]/ul[@class=&quot;dropdown-menu&quot;]/li[1]/a[1]</value>
+      <webElementGuid>97976c01-72c6-40db-91d8-4f42eec9e6b0</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Create Part Number')]</value>
+      <webElementGuid>7aa71489-086f-4ff6-be45-7f7c9428ade7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Catalog'])[1]/following::a[2]</value>
+      <webElementGuid>da5f30a2-fff0-494f-ba64-a59284d4b6d0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='BAPB'])[2]/following::a[4]</value>
+      <webElementGuid>90fa1f57-c577-407c-8f9e-08f0914d1cdd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Create Catalog'])[1]/preceding::a[1]</value>
+      <webElementGuid>6a69fc57-22f4-477b-b965-c2a4ec4881ed</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Create Mapping'])[1]/preceding::a[2]</value>
+      <webElementGuid>ef1a8a80-c240-453f-a320-50e5733c485d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Create Part Number']/parent::*</value>
+      <webElementGuid>d36fe25d-6f6b-4b5e-8f7b-b0a43d5456b8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[@href='http://192.168.3.87:5057/part/create_part_number']</value>
+      <webElementGuid>eaea4e4f-7411-4f72-9ee7-682299b7d00c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[8]/ul/li[2]/ul/li/a</value>
+      <webElementGuid>36108e03-9281-436d-a94e-0ac48b48f18b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'http://192.168.3.87:5057/part/create_part_number' and (text() = 'Create Part Number' or . = 'Create Part Number')]</value>
+      <webElementGuid>b7cea9a2-a0d2-4cc1-8214-733e8446d88e</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
