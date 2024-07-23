37a55675da2533609f64ca82fa5c97f199cc1227
erza.akbar <erza.akbar@503-00342.modena.com>
2024-07-23 09:51:28 +0700

    BRAVA ADD ACCESSORIES

---
+++ Object Repository/website/Page_Masterpiece Retrofridge  MODENA/h1_Sebuah adikarya  terinpirasi dari dapur anda.rs
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h1_Sebuah adikarya  terinpirasi dari dapur anda</name>
+   <tag></tag>
+   <elementGuidId>623d71c8-9b1e-48d9-8be4-c27c79ccba56</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='MasterpieceS3']/div/div/div/div/h1</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.MasterpieceS3_gradient > h1</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=heading[name=&quot;Sebuah adikarya​ terinpirasi dari​ dapur anda.​&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h1</value>
+      <webElementGuid>a90485c5-9a63-465d-a26c-3140843e029a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                            Sebuah adikarya​  terinpirasi dari​ dapur anda.​</value>
+      <webElementGuid>5714df4b-b2b1-4183-82bc-af0597d7f314</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;MasterpieceS3&quot;)/div[@class=&quot;container&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-12 col-lg-7&quot;]/div[@class=&quot;MasterpieceS3_gradient&quot;]/h1[1]</value>
+      <webElementGuid>f884fd60-6f50-4b79-b32c-21d1cf55b972</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='MasterpieceS3']/div/div/div/div/h1</value>
+      <webElementGuid>b4873b4a-f691-4ffe-bd80-853e415343f3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='MODENA Agent'])[1]/following::h1[2]</value>
+      <webElementGuid>e604fce3-9b6c-4596-a8f9-dd584c2cfa85</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[4]/div/div/div/div/h1</value>
+      <webElementGuid>9ce1c31b-3a1d-432d-8cd0-d992b2ef460b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h1[(text() = '
+                            Sebuah adikarya​  terinpirasi dari​ dapur anda.​' or . = '
+                            Sebuah adikarya​  terinpirasi dari​ dapur anda.​')]</value>
+      <webElementGuid>f12b3218-0a41-4b74-b5cc-88e3f6f9234a</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
